bead22b400f3aa27d1aa9d7a49c383701e8b8677
Chitalu Mumba <49457879+chitalu7@users.noreply.github.com>
2022-09-24 13:09:57 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,42 +1,72 @@
 const http = require('http');
-var fs = require('fs'); //NEW* - File System module
+var fs = require('fs'); 
+const url = require("url");
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html'); //Changed 'text/plain' to 'text/html
+  res.setHeader('Content-Type', 'text/html'); 
 
-  fs.readFile('./index.html', null, function(error, data) {  //added 'index.html' as landing page
-    if (error){
-        res.writeHead(404);
-        res.write('File not found!');
-    }else{
-        res.write(data);
-    }
-    res.end();
-  })
+  let parsedURL = url.parse(req.url, true);
+  let path = parsedURL.pathname;
+
+  if (path === '/'){    
+    fs.readFile('./index.html', null, function(error, data) {  
+      if (error){
+          res.writeHead(404);
+          res.write('File not found!');
+      }else{
+          res.write(data);
+      }
+      res.end();
+    })
+  }
+  else if (path === '/ianbeard'){
+    fs.readFile('./ian-beard.html', null, function(error, data) {  
+      if (error){
+          res.writeHead(404);
+          res.write('File not found!');
+      }else{
+          res.write(data);
+      }
+      res.end();
+    })
+  }
+  else if (path === '/ericsergio'){
+    fs.readFile('./eric-sergio.html', null, function(error, data) {  
+      if (error){
+          res.writeHead(404);
+          res.write('File not found!');
+      }else{
+          res.write(data);
+      }
+      res.end();
+    })
+  }
+  else if (path === '/thaianhvu'){
+    fs.readFile('./thaianh-vu.html', null, function(error, data) {  
+      if (error){
+          res.writeHead(404);
+          res.write('File not found!');
+      }else{
+          res.write(data);
+      }
+      res.end();
+    })
+  } else if (path === '/chitalumumba'){
+    fs.readFile('./chitalu-mumba.html', null, function(error, data) {  
+      if (error){
+          res.writeHead(404);
+          res.write('File not found!');
+      }else{
+          res.write(data);
+      }
+      res.end();
+    })
+  }
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
-
-
-/* PREVIOUS CODE 9/24/2022
-const http = require('http');
-var fs = require('fs');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-*/
